0ae15ff21c111a7d376dad544215f1cedc6d87cb
mudpoet <mudpoet@gmail.com>
2025-02-02 16:21:27 -0600

    Update __init__.py

--- components/gm861_uart/__init__.py
+++ components/gm861_uart/__init__.py
@@ -1,1 +1,19 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
 
+DEPENDENCIES = ["uart"]
+CODEOWNERS = ["@mudpoet"]
+
+gm861_uart_ns = cg.esphome_ns.namespace("gm861_uart")
+GM861UARTComponent = gm861_uart_ns.class_("GM861UARTComponent", cg.Component, uart.UARTDevice)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(GM861UARTComponent),
+}).extend(uart.UART_DEVICE_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield uart.register_uart_device(var, config)
