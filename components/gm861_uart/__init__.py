17854173603dd6bcff44419e78497b9ad786353e
mudpoet <mudpoet@gmail.com>
2025-02-03 09:37:50 -0600

    Update __init__.py

--- components/gm861_uart/__init__.py
+++ components/gm861_uart/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, sensor
-from esphome.const import CONF_ID, CONF_HEARTBEAT
+from esphome.components import uart, text_sensor, binary_sensor
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@mudpoet"]
@@ -17,8 +17,10 @@ CONF_HEARTBEAT_STATUS = "heartbeat_status"
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(GM861UARTComponent),
-        cv.Optional(CONF_BARCODE): sensor.sensor_schema(),
-        cv.Optional(CONF_HEARTBEAT_STATUS): sensor.sensor_schema(),
+        cv.Optional(CONF_BARCODE): text_sensor.text_sensor_schema(),
+        cv.Optional(CONF_HEARTBEAT_STATUS): binary_sensor.binary_sensor_schema(
+            device_class="connectivity"
+        ),
     }
 ).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
@@ -28,9 +30,9 @@ def to_code(config):
     yield uart.register_uart_device(var, config)
 
     if CONF_BARCODE in config:
-        barcode_sensor = yield sensor.new_sensor(config[CONF_BARCODE])
+        barcode_sensor = yield text_sensor.new_text_sensor(config[CONF_BARCODE])
         cg.add(var.set_barcode_sensor(barcode_sensor))
 
     if CONF_HEARTBEAT_STATUS in config:
-        heartbeat_sensor = yield sensor.new_sensor(config[CONF_HEARTBEAT_STATUS])
+        heartbeat_sensor = yield binary_sensor.new_binary_sensor(config[CONF_HEARTBEAT_STATUS])
         cg.add(var.set_heartbeat_sensor(heartbeat_sensor))
